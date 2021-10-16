6df073c3e09006193b28127130b3eb1b2cedf7d4
Jorge Sarabia <jorge@sarabiajor.ge>
2021-10-16 17:08:19 -0300

    Update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,6 @@
 // Copyright 2013 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// found in the ../LICENSE file.
 
 // The code in this file (and all other dart files in the package) is
 // analyzed using the rules activated in `analysis_options.yaml`.
